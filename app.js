0b1bd2b8fd4961cb61529f93990d889711e97f91
Usama-Yasir <usamayasir32@gmail.com>
2022-04-17 13:38:21 +0500

    initial commit

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 (function () {
     var names = ["Usama", "Jaun", "Iqbal", "James", "Jerry", "Tom", "Jamaima", "Elia", "Smith", "Jamshed"];
+    
     for (var i = 0; i < names.length; i++) {
-        var firstLetter = names[i].charAt(0).toLowerCase();
-        if (firstLetter === 'j') {
+        var firstLetter = names[i].charAt(0);
+
+        if (firstLetter === 'j' || firstLetter === 'J') {
             greeting_bye.sayGoodbye(names[i]);
         } else {
             greeting_hello.sayHello(names[i]);
