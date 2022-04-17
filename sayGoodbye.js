45da85dfff15dbffb969a096129e1198d6df63dd
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-04-17 13:40:11 +0500

    Delete sayGoodbye.js

--- sayGoodbye.js
+++
@@ -1,8 +0,0 @@
-(function (window) {
-    var greeting_bye = new Object();
-    var greeting = "Good Bye";
-    greeting_bye.sayGoodbye = function (name) {
-        console.log(greeting + " " + name);
-    };
-    window.greeting_bye = greeting_bye;
-  })(window);
