4466f4b98a1b665ba9ae43f3c373926554d7e600
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-04-17 13:39:59 +0500

    Delete sayHello.js

--- sayHello.js
+++
@@ -1,8 +0,0 @@
-(function (window) {
-    var greeting_hello = new Object();
-    var greeting = "Hello";
-    greeting_hello.sayHello = function (name) {
-        console.log(greeting + " " + name);
-    };
-    window.greeting_hello = greeting_hello;
-  })(window);
